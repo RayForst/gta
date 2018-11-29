47f47ba93130be111eb0d6d59e5dbc90136f1e8c
Sergej Kalpakov <sergeikalpakov@gmail.com>
2018-11-29 05:57:12 +0200

    gmaps

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -2,5 +2,16 @@
   .container-fluid
     .row
       .col-xs-12
-        h2 Map
+        GmapMap(
+          :center="{lat:10, lng:10}"
+          :zoom="7"
+          map-type-id="terrain"
+          style="width: 100%; height: 500px")
+          GmapMarker(
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :clickable="true"
+            :draggable="true"
+            @click="center=m.position")
 </template>
--- client/src/main.js
+++ client/src/main.js
@@ -4,9 +4,17 @@ import router from "./router";
 import { sync } from "vuex-router-sync";
 import store from "./store";
 import "./assets/css/vendors/layout/flexboxgrid.css";
+import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.config.productionTip = false;
 sync(store, router);
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBiqPVo8Anjm534FSZ4-89L1H1yZxfQWvU",
+    libraries: "places"
+  }
+});
 
 new Vue({
   router,
