e98d1e1555e057f4de53190e075dbebb6fa79153
Sergej Kalpakov <sergeikalpakov@gmail.com>
2018-12-12 20:27:29 +0200

    morphing changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,11 +3,6 @@
       app-burger
       app-header
       section.content
-        <div class="morph-wrap">
-          <svg class="morph" width="1400" height="770" viewBox="0 0 1400 770">
-            <path d="M 415.6,206.3 C 407.4,286.6 438.1,373.6 496.2,454.8 554.3,536.1 497,597.2 579.7,685.7 662.4,774.1 834.3,731.7 898.5,653.4 962.3,575 967.1,486 937.7,370 909.3,253.9 937.7,201.5 833.4,105.4 729.3,9.338 602.2,13.73 530.6,41.91 459,70.08 423.9,126.1 415.6,206.3 Z"/>
-          </svg>
-        </div>
         router-view(v-if="isLoaded")
       app-footer
 </template>
@@ -17,7 +12,7 @@ import appBurger from "@/components/ui/Burger";
 import appHeader from "@/components/Header";
 import appFooter from "@/components/Footer";
 import contentService from "@/services/ContentService";
-import Morph from '@/scripts/demo2.js'
+import Morph from "@/scripts/demo2.js";
 
 export default {
   metaInfo: {
@@ -51,9 +46,9 @@ export default {
   mounted() {
     //this.isLoaded = true;
     setTimeout(function() {
-      console.log('trying to morph')
-      Morph()
-    }, 3000)
+      console.log("trying to morph");
+      Morph();
+    }, 3000);
   }
 };
 </script>
