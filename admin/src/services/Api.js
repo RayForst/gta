2a9b37fcab06faae08c688f8d8edb5e76ac55bc5
Sergej Kalpakov <sergeikalpakov@gmail.com>
2019-02-27 13:35:16 +0200

    jwt token validation

--- admin/src/services/Api.js
+++ admin/src/services/Api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
+import store from "./../store";
 
 export default () => {
-  return axios.create({
+  const token = store.state.token;
+  let config = {
     baseURL: 'http://localhost:6680/api/'
-  });
+  };
+
+  if (token) {
+    config.headers = {
+      'Authorization': token
+    };
+  }
+  
+  return axios.create(config);
 };
